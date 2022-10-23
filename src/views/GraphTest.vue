72595047ae4cca1c52fa5c77fa6226daa6c95f93
Jona898 <jonathan.schaeberle@outlook.de>
2022-10-23 22:54:25 +0200

    Added Vue Router

---
+++ src/views/GraphTest.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import elkjsTest from "../components/graph/elkjsTest.vue";
+</script>
+
+<template>
+  <div>
+    <elkjsTest></elkjsTest>
+  </div>
+</template>
+
+<style scoped></style>
